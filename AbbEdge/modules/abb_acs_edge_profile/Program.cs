67911eddd902389b578126b7562017c3e6422b3b
Maxim Khlupnov <m.khlupnov@gmail.com>
2018-06-28 16:08:56 +0300

    Update to multiple modules

--- AbbEdge/modules/abb_acs_edge_profile/Program.cs
+++ AbbEdge/modules/abb_acs_edge_profile/Program.cs
@@ -1,4 +1,4 @@
-namespace AbbAcsEdgeModule
+namespace abb_acs_edge_profile
 {
     using System;
     using System.IO;
@@ -17,13 +17,7 @@ namespace AbbAcsEdgeModule
 
         static void Main(string[] args)
         {
-            // The Edge runtime gives us the connection string we need -- it is injected as an environment variable
-            string connectionString = Environment.GetEnvironmentVariable("EdgeHubConnectionString");
-
-            // Cert verification is not yet fully functional when using Windows OS for the container
-            bool bypassCertVerification = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-            if (!bypassCertVerification) InstallCert();
-            Init(connectionString, bypassCertVerification).Wait();
+            Init().Wait();
 
             // Wait until the app unloads or is cancelled
             var cts = new CancellationTokenSource();
@@ -43,49 +37,16 @@ namespace AbbAcsEdgeModule
         }
 
         /// <summary>
-        /// Add certificate in local cert store for use by client for secure connection to IoT Edge runtime
-        /// </summary>
-        static void InstallCert()
-        {
-            string certPath = Environment.GetEnvironmentVariable("EdgeModuleCACertificateFile");
-            if (string.IsNullOrWhiteSpace(certPath))
-            {
-                // We cannot proceed further without a proper cert file
-                Console.WriteLine($"Missing path to certificate collection file: {certPath}");
-                throw new InvalidOperationException("Missing path to certificate file.");
-            }
-            else if (!File.Exists(certPath))
-            {
-                // We cannot proceed further without a proper cert file
-                Console.WriteLine($"Missing path to certificate collection file: {certPath}");
-                throw new InvalidOperationException("Missing certificate file.");
-            }
-            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
-            store.Open(OpenFlags.ReadWrite);
-            store.Add(new X509Certificate2(X509Certificate2.CreateFromCertFile(certPath)));
-            Console.WriteLine("Added Cert: " + certPath);
-            store.Close();
-        }
-
-
-        /// <summary>
-        /// Initializes the DeviceClient and sets up the callback to receive
+        /// Initializes the ModuleClient and sets up the callback to receive
         /// messages containing temperature information
         /// </summary>
-        static async Task Init(string connectionString, bool bypassCertVerification = false)
+        static async Task Init()
         {
-            Console.WriteLine("Connection String {0}", connectionString);
-
             MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
-            // During dev you might want to bypass the cert verification. It is highly recommended to verify certs systematically in production
-            if (bypassCertVerification)
-            {
-                mqttSetting.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
-            }
             ITransportSettings[] settings = { mqttSetting };
 
             // Open a connection to the Edge runtime
-            DeviceClient ioTHubModuleClient = DeviceClient.CreateFromConnectionString(connectionString, settings);
+            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
             await ioTHubModuleClient.OpenAsync();
             Console.WriteLine("IoT Hub module client initialized.");
 
@@ -102,8 +63,8 @@ namespace AbbAcsEdgeModule
         {
             int counterValue = Interlocked.Increment(ref counter);
 
-            var deviceClient = userContext as DeviceClient;
-            if (deviceClient == null)
+            var moduleClient = userContext as ModuleClient;
+            if (moduleClient == null)
             {
                 throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
             }
@@ -119,7 +80,7 @@ namespace AbbAcsEdgeModule
                 {
                     pipeMessage.Properties.Add(prop.Key, prop.Value);
                 }
-                await deviceClient.SendEventAsync("output1", pipeMessage);
+                await moduleClient.SendEventAsync("output1", pipeMessage);
                 Console.WriteLine("Received message sent");
             }
             return MessageResponse.Completed;
